0a13ee84b6f50b31ffeb7c3b067f2dcecda20db9
hezijie <lonegunmanb@hotmail.com>
2024-09-20 12:20:51 +0800

    amdending missing modtm provider

--- v4/terraform_override.tf
+++ v4/terraform_override.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+    modtm = {
+      source = "Azure/modtm"
+      version = ">= 0.3.2, < 1.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.5.0"
