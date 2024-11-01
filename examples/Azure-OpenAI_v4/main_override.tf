012d34ad1bc073c1986d920d7b0cfb88cbc8b622
soferreira <soferreira@microsoft.com>
2024-11-01 11:23:35 +0100

    remove random

--- examples/Azure-OpenAI_v4/main_override.tf
+++ examples/Azure-OpenAI_v4/main_override.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0, < 4.0.0"
-    }
   }
 }
 
