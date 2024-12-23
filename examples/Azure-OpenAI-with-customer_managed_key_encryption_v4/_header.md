5fbf15d34685c3e8e3652a3c28e67533f118b2a4
hezijie <lonegunmanb@hotmail.com>
2024-12-23 09:46:17 +0800

    bump whole module into v4

--- examples/Azure-OpenAI-with-customer_managed_key_encryption_v4/_header.md
+++
@@ -1,3 +0,0 @@
-# Default example
-
-This deploys an Azure OpenAI service with a customer managed key.
--- terraform.tf
+++ terraform.tf
@@ -5,10 +5,9 @@ terraform {
       source  = "Azure/azapi"
       version = "~> 2.0"
     }
-    # TODO: Ensure all required providers are listed here.
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0, < 4.0"
+      version = "~> 4.0"
     }
     modtm = {
       source  = "Azure/modtm"
